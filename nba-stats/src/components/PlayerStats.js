4c9fac8f216c4007401479d6aa1567c122fe8bb0
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-15 16:14:13 -0600

    added position and team to PlayerStats

--- nba-stats/src/components/PlayerStats.js
+++ nba-stats/src/components/PlayerStats.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const PlayerStats = props => {
     return (
-        <h3>Height: {props.player.height_feet} feet {props.player.height_inches} inches</h3>
+        <div>
+            <h3>Height: {props.player.height_feet} feet {props.player.height_inches} inches</h3>
+            <h3>Position: {props.player.position}</h3>
+            <h3>Team: {props.player.team.full_name}</h3>
+        </div>
     )
 }
 
