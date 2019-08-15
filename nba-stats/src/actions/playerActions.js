095ffca29d336bd33961fa492bac48a2cfd3e97f
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-15 15:08:31 -0600

    working on actions

---
+++ nba-stats/src/actions/playerActions.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const GET_PLAYER_DATA_START = 'GET_PLAYER_DATA_START'
+export const GET_PLAYER_DATA_SUCCESS = 'GET_PLAYER_DATA_SUCCESS'
+export const  GET_PLAYER_DATA_FAILURE = 'GET_PLAYER_DATA_FAILURE' 
