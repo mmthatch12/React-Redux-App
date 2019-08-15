c2aef8c374967b366ee4dd756bcb1d397fa08bd9
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-15 14:12:52 -0600

    build dummy reducer, set up redux stuff in index.js

---
+++ nba-stats/src/reducer/playerReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    players: []
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
