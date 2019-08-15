7f5d837b589b0114b08ffe1fa7626fcb50e43981
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-15 16:04:14 -0600

    created PlayerStats.js to house player stats

--- nba-stats/src/components/Player.js
+++ nba-stats/src/components/Player.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Player = props => {
     return (
         <div>
-            <h1>{props.player.first_name}</h1>
+            <h2>{props.player.first_name} {props.player.last_name}</h2>
+            
         </div>
         
     )
---
+++ nba-stats/src/components/PlayerStats.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlayerStats = props => {
+    return (
+        <h3>{props.height_feet} {props.height_inches}</h3>
+    )
+}
+
+export default PlayerStats
